b2e1d859e094c3a57a9eac47f92c67b6e5c68e0f
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-26 23:21:12 +0100

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.jsx'
-import './todo.css'
-
+import './todo3.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
