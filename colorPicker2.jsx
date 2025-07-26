046c9726b23923925f4152a9e602f1b935c0353d
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-26 23:22:09 +0100

    Add files via upload

--- colorPicker2.jsx
+++ colorPicker2.jsx
@@ -2,36 +2,42 @@ import React from "react";
 
 function ColorPicker(){
 
-      const [color, setColor] = React.useState("#FFFFFF");
+  const [color, setColor] = React.useState(()=>{
 
-      function handleColorChange(event){
-        setColor(event.target.value);
-      }
+    const saved = localStorage.getItem('color');
 
+       return saved ? JSON.parse(saved) : 
+          "#FFFFFF"
+  }
+  );
 
-   return (
-    <>
-       <div className="color-content">
-        <h2>Color Picker</h2>
+  function saveColor(event){
+   setColor(event.target.value);
+    
+   localStorage.setItem('color', JSON.stringify(event.target.value));
+  }
 
-        <div className="display-color" style={{backgroundColor : color}}>
-                
-                <p className="color-display">Selected Color : {color}</p>
-        </div>
+    return(
+      <>
+         <div className="color-div">
+
+            <h1>Color Picker</h1>
+
+            <div className="display-color" style={{backgroundColor : color}}>
+               <p className="color-display">Selected color : {color}</p>
+            </div>
 
             <label>Select a color : </label>
 
             <input 
             type="color" 
-            className="input-color"
             value={color}
-            onChange={handleColorChange}
+            onChange={saveColor}
+            className="color"
             />
-       </div>
-
-
-    </>
-   );
+         </div>
+      </>
+    )
 }
 
 
