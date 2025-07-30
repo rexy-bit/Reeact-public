b35fef190dadc7cb1802a505bf192072fa1b759d
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-30 22:40:25 +0100

    Add files via upload

--- Main.jsx
+++ Main.jsx
@@ -1,42 +1,39 @@
-import React, { useEffect } from "react"
-import Header from "./Header"
-import MemeInputs from "./MemeInputs"
-import Meme from "./Meme"
+import React from "react";
+import Header from "./Header";
+import MemeInputs from "./MemeInputs";
+import Meme from "./Meme";
 export default function Main(){
 
-    const [meme, setMeme] = React.useState(
-        {
-            imageSrc:"http://i.imgflip.com/1bij.jpg",
-            topText:"Some thing different",
-            bottomText:"Walk into Mordor"
-        }
-    )
-
+    const [meme, setMeme] = React.useState({
+        topText : "Some thing different",
+        imageUrl : "http://i.imgflip.com/1bij.jpg",
+        bottomText : "Walk into Mordor"
+    });
+    
     const [allMemes, setAllMemes] = React.useState([]);
+
     React.useEffect(()=>{
-         fetch("https://api.imgflip.com/get_memes")
-          .then(res => res.json())
-            .then((data) =>{
-                  console.log(data.data.memes);
-                  setAllMemes(data.data.memes);
-            } )
+        fetch('https://api.imgflip.com/get_memes')
+          .then(res=> res.json()) 
+             .then((data)=>{
+                setAllMemes(data.data.memes);
+             });
     }, []);
 
-
     return(
-        <>
-          <main>
+        <main>
+
             <Header/>
             <MemeInputs
             meme={meme}
             setMeme={setMeme}
             allMemes={allMemes}
+            setAllMemes={setAllMemes}
+
             />
             <Meme
              meme={meme}
-             setMeme={setMeme}
             />
-          </main>
-        </>
+        </main>
     )
 }
