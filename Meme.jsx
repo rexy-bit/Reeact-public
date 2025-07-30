9817e9a164d4da2b7b81d48cc85bb2a58de607c2
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-30 22:40:33 +0100

    Add files via upload

--- Meme.jsx
+++ Meme.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 export default function Meme(props){
 
+    
     return(
+
         <div className="meme">
-            <img src={props.meme.imageSrc} alt="meme image" className="meme-image"/>
-                <span className="top">{props.meme.topText}</span>
-                <span className="bottom">{props.meme.bottomText}</span>
+            <img 
+            src={props.meme.imageUrl} 
+            alt="meme image" 
+            className="image" />
+
+            <span className="top">{props.meme.topText}</span>
+            <span className="bottom">{props.meme.bottomText}</span>
         </div>
     );
 }
