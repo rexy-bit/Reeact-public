81df43ee56012006075900567824f524fcae0fe0
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-07-30 22:41:45 +0100

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import trollImage from '../assets/troll-face.png'
+import React from "react";
+import chefImage from '../assets/chef-claude-icon.png'
 export default function Header(){
 
     return(
         <header>
-            <img src={trollImage} alt="troll image" className='troll-image'
-             width="70"
+            <img 
+            src={chefImage} 
+            alt="chef image" 
+            width="70"
             />
 
-            <h1>Meme Generator</h1>
+            <h1>Chef Gaston 2</h1>
         </header>
     );
 }
