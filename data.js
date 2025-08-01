04b02676787af21ea41b7b80f069cb01dcd4b204
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-01 21:55:52 +0100

    Add files via upload

--- data.js
+++ data.js
@@ -1,38 +1,115 @@
-export default [
-    {
-        id: 1,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-japan-image-url",
-            alt: "Mount Fuji"
-        },
-        title: "Mount Fuji",
-        country: "Japan",
-        googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
-        dates: "12 Jan, 2021 - 24 Jan, 2021",
-        text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-    },
-    {
-        id: 2,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-australia-image-url",
-            alt: "Sydney Opera House"
-        },
-        title: "Sydney Opera House",
-        country: "Australia",
-        googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
-        dates: "27 May, 2021 - 8 Jun, 2021",
-        text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."
-    },
-    {
-        id: 3,
-        img: {
-            src: "https://scrimba.com/links/travel-journal-norway-image-url",
-            alt: "Geirangerfjord"
-        },
-        title: "Geirangerfjord",
-        country: "Norway",
-        googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
-        dates: "01 Oct, 2021 - 18 Nov, 2021",
-        text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
-    },
-]
+import marcAurele from "./assets/marc-aurele.jpg"
+import ceasar from "./assets/ceasar.jpg"
+import napoleon from "./assets/napoleon.jpg"
+import alexander from "./assets/alexander.jpg"
+import achilles from "./assets/achilles.jpg"
+import alexander2 from "./assets/alexander2.jpg"
+import alexander3 from "./assets/alexander3.jpg"
+import ceasar2 from "./assets/ceasar2.jpg"
+import leonidas from "./assets/leonidas.jpg"
+import leonidas2 from "./assets/leonidas2.jpg"
+import augustus from "./assets/augustus.jpg"
+import marc2 from "./assets/marc2.jpg"
+import achilles2 from "./assets/achilles2.jpg"
+import achilles3 from "./assets/achilles3.jpg"
+export default  [
+  {
+    text: "You have power over your mind – not outside events. Realize this, and you will find strength.",
+    author: "Marcus Aurelius",
+    image: marcAurele,
+    origin: "Meditations",
+    era: "Stoicism"
+  },
+  {
+    text: "Veni, vidi, vici.",
+    author: "Julius Caesar",
+    image: ceasar,
+    origin: "Letter to the Roman Senate",
+    era: "Rome"
+  },
+  {
+    text: "Glory is fleeting, but obscurity is forever.",
+    author: "Napoleon Bonaparte",
+    image: napoleon,
+    origin: "Speech",
+    era: "France"
+  },
+  {
+    text: "There is nothing impossible to him who will try.",
+    author: "Alexander the Great",
+    image: alexander,
+    origin: "Legend",
+    era: "Greece"
+  },{
+  text: "Come and take them.",
+  author: "Leonidas I",
+  image: leonidas,
+  origin: "Battle of Thermopylae",
+  era: "Sparta"
+},
+{
+  text: "Let none forget how menacing we are. We are lions.",
+  author: "Achilles",
+  image: achilles,
+  origin: "The Iliad (Homer)",
+  era: "Mythology"
+},
+{
+  text: "I found Rome a city of bricks and left it a city of marble.",
+  author: "Augustus",
+  image: augustus,
+  origin: "Attributed",
+  era: "Rome"
+},
+{
+  text: "The happiness of your life depends upon the quality of your thoughts.",
+  author: "Marcus Aurelius",
+  image: marc2,
+  origin: "Meditations",
+  era: "Stoicism"
+},{
+  text: "I am not afraid of an army of lions led by a sheep; I am afraid of an army of sheep led by a lion.",
+  author: "Alexander the Great",
+  image: alexander2,
+  origin: "Attributed",
+  era: "Greece"
+},
+{
+  text: "A tomb now suffices him for whom the whole world was not sufficient.",
+  author: "Alexander the Great",
+  image: alexander3,
+  origin: "After his death",
+  era: "Greece"
+},{
+  text: "It is better to create than to learn! Creating is the essence of life.",
+  author: "Julius Caesar",
+  image: ceasar2,
+  origin: "Attributed",
+  era: "Rome"
+},
+{
+  text: "I’d rather be a servant to a poor man than rule all the dead.",
+  author: "Achilles",
+  image: achilles3,
+  origin: "Homer, The Iliad",
+  era: "Mythology"
+},
+{
+  text: "Let me not then die ingloriously and without a struggle, but let me first do some great thing.",
+  author: "Achilles",
+  image: achilles2,
+  origin: "The Iliad, Book 22",
+  era: "Mythology"
+},
+
+{
+  text: "Cowards die many times before their deaths; the valiant never taste of death but once.",
+  author: "Julius Caesar",
+  image: ceasar,
+  origin: "Shakespeare’s Julius Caesar",
+  era: "Rome"
+}
+
+
+
+];
