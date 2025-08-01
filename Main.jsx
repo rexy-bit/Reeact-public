90a471b1313ccf1aa236f6558758b022cd4a83d9
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-01 21:56:39 +0100

    Add files via upload

--- Main.jsx
+++ Main.jsx
@@ -1,49 +1,31 @@
 import React from "react";
-import WeatherInputs from "./WeatherInputs";
-import WeatherDisplay from "./WeatherDisplay";
-
+import Quotes from "./Quotes";
 export default function Main(){
 
-    const [city, setCity] = React.useState(()=>{
-        const saved = localStorage.getItem('city');
+    const [currentQuote, setCurrentQuote] = React.useState(()=>{
+        const saved = localStorage.getItem('currentQuote');
 
         return saved ? JSON.parse(saved) : 
-          'Algiers'
+           0
     });
 
     React.useEffect(()=>{
-        localStorage.setItem('city', JSON.stringify(city));
-    }, [city]);
+        localStorage.setItem('currentQuote', JSON.stringify(currentQuote));
+    }, [currentQuote]);
 
-    const [weather, setWeather] = React.useState(()=>{
-        const saved = localStorage.getItem('weather');
-
-        return saved ? JSON.parse(saved) : 
-           null
-    });
-
-    React.useEffect(()=>{
-        localStorage.setItem('weather', JSON.stringify(weather));
-    }, [weather]);
 
 
     return(
-        <main>
-            
-            <h1>Weather App</h1>
 
-            <WeatherInputs
-            city={city}
-            setCity={setCity}
-            weather={weather}
-            setWeather={setWeather}
-
-            />
-
-            <WeatherDisplay
-             weather={weather}
-             setWeather={setWeather}
-            />
+        <main> 
+            <h1>Wisdom of Legends 🪶</h1>
+       <Quotes
+        currentQuote={currentQuote}
+        setCurrentQuote={setCurrentQuote}
+        />
         </main>
+
     )
+    
+
 }
