ca3bafa28c2c9e5787308322fb7ecd65394fa19e
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-01 21:57:56 +0100

    Add files via upload

--- Quotes.jsx
+++ Quotes.jsx
@@ -4,29 +4,49 @@ export default function Quotes({currentQuote, setCurrentQuote}){
 
     const q = data[currentQuote];
     const [fadeKey, setFadeKey] = React.useState(0);
-    
-    function getNextQuote(){
 
-        if(currentQuote === data.length - 1){
+    function goRight(){
+
+        if(currentQuote === (data.length - 1)){
             setCurrentQuote(0);
         }else{
-        setCurrentQuote(prev => prev + 1);
+            setCurrentQuote(prev => prev + 1);
         }
 
         setFadeKey(prev => prev + 1);
     }
 
+    function goLeft(){
+        if(currentQuote === 0){
+            setCurrentQuote(data.length - 1);
+        }else{
+            setCurrentQuote(prev => prev - 1);
+        }
+
+        setFadeKey(prev => prev + 1);
+    }
+    
     return(
         <div className="quote fade-in" key={fadeKey}>
-           <img src={q.image} alt={q.author} className="image zoom"/>   
-           <div className="others">     
-           <div className="citation">{q.text}</div>
-           <div className="author">- {q.author}</div>
-           <div className="source">Source : {q.origin}</div>
-           </div>
 
-         <button className="next-button" onClick={getNextQuote}>Next</button>
+            <img src={q.image} alt={q.author} className="image zoom"/>
+
+            <div className="others" >
+                <div className="citation">{q.text}</div>
+                <div className="author">- {q.author}</div>
+                <div className="source">Source : {q.origin}</div>
+                <div className="era">Era : {q.era}</div>
+
+            </div>
+
+            <div className="buttons">
+                <div className="left" onClick={goLeft}>&#10094;</div>
+                <div className="right" onClick={goRight}>&#10095;</div>
+            </div>
+
+            <p className="current">Quote {currentQuote + 1}/{data.length}</p>
+
         </div>
-
     );
+    
 }
