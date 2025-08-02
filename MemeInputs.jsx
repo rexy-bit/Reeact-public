95e3f449e86fd1d9dd5123dcf36eab02ee0bb1cf
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-02 22:31:06 +0100

    Add files via upload

--- MemeInputs.jsx
+++ MemeInputs.jsx
@@ -1,79 +1,82 @@
 import React from "react";
 
-export default function MemeInputs(props){
 
-     const [topText, setTopText] = React.useState('');
-     const [bottomText, setBottomText] = React.useState('');
+export default function MemeInputs({meme, setMeme, allMeme, setAllMeme}){
+    
+    const [inputTop, setInputTop] = React.useState('');
+    const [inputBottom, setInputBottom] = React.useState('');
 
-     function saveTopText(event){
+    function saveTopText(event){
+
         const value = event.target.value;
 
-         setTopText(value);
+        setInputTop(value);
 
-         props.setMeme(prev=>({
+        setMeme(prev => ({
             ...prev,
             topText : value
-         }));
+        }));
 
-     }
+    }
 
-     function saveBottomText(event){
+    
+
+    function saveBottomText(event){
 
         const value = event.target.value;
 
-        setBottomText(value);
+        setInputBottom(value);
 
-        props.setMeme(prev=>({
-            ...prev,
-            bottomText: value
+        setMeme(prev => ({
+            ...prev, 
+            bottomText : value
         }));
-     }
 
-     function getImage(){
+    }
 
-        const randomIndex = Math.floor(Math.random() * props.allMemes.length);
-        const newImageUrl = props.allMemes[randomIndex].url;
+    function getImage(){
 
-        props.setMeme(prev=>({
+        console.log(allMeme);
+
+        const randomIndex = Math.floor(Math.random() * (allMeme.length - 1));
+
+        const image = allMeme[randomIndex].url;
+
+        setMeme(prev => ({
             ...prev,
-            imageUrl : newImageUrl
+            image : image
         }));
-     }
+    }
+    return(
+        <div className="inputs">
 
-       return(
+          <div className="inp">
+            <label htmlFor="">Top Text
+            <input 
+            type="text" 
+            className="in" 
+            placeholder="Some thing different"
+            value={inputTop}
+            onChange={saveTopText}
+            />
+            </label>
 
-           <div className="inputs">
- 
-             <div className="labels">
-              <label>Top Text
-                 <input 
-                 type="text" 
-                 className="in" 
-                 name="top"
-                 placeholder="Some thing different"
-                 value={topText}
-                 onChange={saveTopText}
-                 />
-              </label>
+            <label htmlFor="">Bottom Text
+                <input 
+                type="text" 
+                className="in" 
+                placeholder="Walk into Mordor"
+                value={inputBottom}
+                onChange={saveBottomText}
+                />
+            </label>
+          </div>
 
-              <label>Bottom Text
-
-                  <input 
-                  type="text" 
-                  name="bottom"
-                  className="in"
-                  placeholder="Walk into Mordor"
-                  value={bottomText}
-                  onChange={saveBottomText}
- 
-                  />
-              </label>
-              </div>
-
-              <button 
-              className="get-button"
-              onClick={getImage}
-              >Get a new meme image</button>
-           </div>
-       );
+          <button 
+          className="get-button"
+          onClick={getImage}
+          >Get a new meme image</button>
+          
+        </div>
+    );
 }
