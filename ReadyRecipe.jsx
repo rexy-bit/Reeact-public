ea2a35be3e618aaccb4cc66785c44446d816e62d
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-02 22:33:17 +0100

    Add files via upload

--- ReadyRecipe.jsx
+++ ReadyRecipe.jsx
@@ -1,32 +1,29 @@
 import React from "react";
 import { getRecipeFromGemini } from "../ai";
-export default function ReadyRecipe(props){
+export default function ReadyRecipe({ingredients, setIngredients, recipe, setRecipe, ref}){
 
     const [isLoading, setIsLoading] = React.useState(false);
-
     async function getRecipe(){
-          setIsLoading(true);
-          const generatedRecipe = await getRecipeFromGemini(props.ingredients);
+         
+        setIsLoading(true);
 
-          props.setRecipe(generatedRecipe);
-          console.log(generatedRecipe);
-          setIsLoading(false);
+        const generatedRecipe = await getRecipeFromGemini(ingredients);
+        console.log(generatedRecipe);
+        setRecipe(generatedRecipe);
+
+         setIsLoading(false);
     }
 
-
     return(
-        props.ingredients.length  > 3 && <div className="ready-recipe">
-
-            <div className="ready-div" ref={props.ref}>
+        ingredients.length > 3 && <div className="ready-recipe" ref={ref}>
+            <div className="ready">
                 <h3>Ready for a recipe?</h3>
                 <p className="generate">Generate a recipe from your list of ingredients</p>
             </div>
 
-            <button 
-            className="get-button"
-            onClick={getRecipe}
-            >
-                {isLoading ? "Loading..." : "Get a recipe"}
+            <button className="get-button" onClick={getRecipe}>
+                
+                {isLoading ? "...Loading" : "Get a recipe"}
             </button>
         </div>
     );
