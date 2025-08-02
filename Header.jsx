b328ab7ac94ba978ed8fb31987b60a277986c424
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-02 22:30:18 +0100

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import trollImage from '../assets/troll-face.png'
+import React from "react";
+import troll from "../assets/troll-face.png"
 export default function Header(){
-
+      
     return(
         <header>
-            <img src={trollImage} alt="troll image" className='troll-image'
-             width="70"
-            />
+            <img src={troll} alt="troll-face" className="troll-face" width="70"/>
 
             <h1>Meme Generator</h1>
         </header>
