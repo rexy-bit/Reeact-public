26f6eb0dd01c67f44523af3653c79806b3ba4791
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-02 22:32:50 +0100

    Add files via upload

--- IngredientsDisplay.jsx
+++ IngredientsDisplay.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 
-export default function IngredientsDisplay(props){
+export default function IngredientsDisplay({ingredients, setIngredients}){
 
     function removeIngredient(index){
-        const newIngredients = props.ingredients.filter((_,i)=> i !== index);
+        const newIngredients = ingredients.filter((_,i)=>i !== index);
 
-
-        props.setIngredients(newIngredients);
+        setIngredients(newIngredients);
     }
 
     return(
 
-        <div className="display-ingredients">
-            <h2>Ingredients in hand:</h2>
+       <div className="display-ingredients">
+        <h2>Ingredients on hand:</h2>
+        <div className="ingredients">
 
-            <div className="ingredients">
-                {props.ingredients.map((ingredient,i)=>{
-                    return(
-                        <li key={i} onClick={()=>{removeIngredient(i)}}>{ingredient}</li>
-                    )
-                })}
-            </div>
+           {ingredients.map((ingredient,i)=>{
+            return(
+              <li key={i} onClick={()=>{removeIngredient(i)}}>{ingredient}</li>
+            );
+           })}
+        </div>
         </div>
     );
 }
