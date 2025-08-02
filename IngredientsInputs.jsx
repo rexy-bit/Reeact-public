7eb5bf1478743c0048c8d3e56e01d906eb36681f
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-02 22:32:58 +0100

    Add files via upload

--- IngredientsInputs.jsx
+++ IngredientsInputs.jsx
@@ -1,38 +1,76 @@
 import React from "react";
 
-export default function IngredientsInputs(props){
+export default function IngredientsInputs({ingredients, setIngredients}){
+
+    const [msg, setMsg] = React.useState({color : "red", show : false, text : ''});
+
 
     function addIngredient(formData){
-        const newIngredient = formData.get("ingredient");
-        const trouve = props.ingredients.find((ing)=> ing.toLowerCase() === newIngredient.toLowerCase());
 
-        if(!newIngredient || trouve){
-            return;
+        const newIngredient = formData.get("ingredient");
+
+        let trouve = ingredients.find((ing)=> ing.toLowerCase() === newIngredient.toLowerCase());
+
+        if(!newIngredient){
+           setMsg(prev=> ({
+            ...prev,
+            show : true, text : 'Please enter an ingredient before submitting'
+           }));
+
+           setTimeout(()=>{
+            setMsg(prev=> ({
+            ...prev,
+            show : false
+           }));
+
+           }, 1500);
+
+           return;
+        }else if(trouve){
+
+            setMsg(prev=> ({
+            ...prev,
+            show : true, text : 'The ingredient already exists'
+           }));
+
+           setTimeout(()=>{
+            setMsg(prev=> ({
+            ...prev,
+            show : false
+           }));
+
+           }, 1500);
+
+           return;
+
         }
 
-        const newIngredients = [...props.ingredients, newIngredient];
-        
-        props.setIngredients(newIngredients);
-        
+        const newIngredients = [...ingredients, newIngredient];
+
+        setIngredients(newIngredients);
+
+
     }
 
-    
 
     return(
 
-        <form className="inputs" action={addIngredient}>
-            
+        <form action={addIngredient}>
+
+           <div className="inputs">
             <input 
             type="text" 
-            className="in"
+            className="in" 
             name="ingredient"
-            placeholder="e.g. basilic"
+            placeholder="e.g parsley"
             />
 
-            <button 
-            className="add-button"
-               
-            >+ add ingredient</button>
+            <button className="add-button">+ add ingredient</button>
+            </div>
+
+            {msg.show && 
+              <p className="msg" style={{color : msg.color}}>{msg.text}</p>
+            }
         </form>
     );
 }
