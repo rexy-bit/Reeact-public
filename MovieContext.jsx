2ec969c8b22bec21978282743dedd835622ad817
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-03 23:24:02 +0100

    Add files via upload

--- MovieContext.jsx
+++ MovieContext.jsx
@@ -2,34 +2,34 @@ import React from "react";
 
 const MovieContext = React.createContext();
 
-export const useMovieContext = ()=> React.useContext(MovieContext)
+export const useMovieContext = ()=> React.useContext(MovieContext);
 
-export const MovieProvider = ({children})=>{
+export const MovieProvider = ({children}) =>{
 
-    const [favorites, setFavorites] = React.useState([]);
+    const [favorites, setFavorites] = React.useState(()=>{
+        const saved = localStorage.getItem('favorites');
 
-    React.useEffect(()=>{
-        const storedFavs = localStorage.getItem("favorites");
+        return saved ? JSON.parse(saved) : 
+           []
+    });
 
-        if(storedFavs) setFavorites(JSON.parse(storedFavs));
-    } , []);
+
 
     React.useEffect(()=>{
         localStorage.setItem('favorites', JSON.stringify(favorites));
     }, [favorites]);
 
-    const addToFavorites = (movie)=>{
+    const addToFavorites = (movie) => {
+      
         setFavorites(prev => [...prev, movie]);
     }
 
-    const removeFromFavorites = (movieId)=>{
-         
-         setFavorites(prev => prev.filter(movie => movie.id !== movieId));
+    const removeFromFavorites = (movieId) =>{
+        setFavorites(prev => prev.filter((movie)=> movie.id !== movieId));
     }
 
     const isFavorite = (movieId)=>{
-
-        return favorites.some(movie=> movie.id === movieId);
+       return favorites.some((m)=> m.id === movieId);
     }
 
     const value = {
@@ -37,9 +37,12 @@ export const MovieProvider = ({children})=>{
         addToFavorites,
         removeFromFavorites,
         isFavorite
-    }
+    };
 
-    return <MovieContext.Provider value={value}>
-        {children}
-    </MovieContext.Provider>
+    return(
+        <MovieContext.Provider value={value}>
+             {children}
+
+        </MovieContext.Provider>
+    )
 }
