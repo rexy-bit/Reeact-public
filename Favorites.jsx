2e15978ac3af7868de47aeafbdb8e79ea96b40c3
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-03 23:23:31 +0100

    Add files via upload

--- Favorites.jsx
+++ Favorites.jsx
@@ -1,34 +1,39 @@
 import React from "react";
-import '../css/Favorites.css'
 import { useMovieContext } from "../Contexts/MovieContext";
 import MovieCard from "../Components/MovieCard";
+import '../css/Favorites.css'
 export default function Favorites(){
 
     const {favorites} = useMovieContext();
 
-    if(favorites){
-        return     (     
-                <div className="favorites">   
-
-                  <h2>Favorites</h2>
-                   <div className="movies-grid">
-                        {favorites.map((movie)=>{
-                             return(
-                          (<MovieCard
-                                movie={movie}
-                                key={movie.id}
-                                />)
-                             )
-                        })}
-                    </div>
-                    </div>
-        )
-    }
+    if(!favorites || favorites.length === 0){
     return(
 
-        <div className="favorites-empty">
+            <div className="favorites-empty">
             <h2>No favorite Movies Yet</h2>
             <p>Start adding movies to your favorites and they will appear here</p>
         </div>
-    )
+    );
+    }
+
+    if(favorites){
+        return (
+        <div className="favorites">
+            <h3>Favorites</h3>
+
+            <div className="movies-grid">
+                {favorites.map((movie)=>{
+                    return(
+                        <MovieCard
+                        movie={movie}
+                        key={movie.id}
+                        />
+                    )
+                })}
+            </div>
+        </div>
+        )
+    }
+
+
 }
