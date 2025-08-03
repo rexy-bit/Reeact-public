38e9b4b863958104217d5da8eba8e38cd87d94c5
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-03 23:23:18 +0100

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import App from './App.jsx'
 import './css/index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { MovieProvider } from './Contexts/MovieContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
+    
   </StrictMode>,
 )
