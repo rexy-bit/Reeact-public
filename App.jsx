f22e2cd8af601a1df644691f2b8c21185d306429
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-03 23:22:53 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
-import Main from './Components/Main'
-import { BrowserRouter } from 'react-router-dom';
-import './css/App.css'
-function App() {
-   
-  return(
+import React from 'react'
 
-    <BrowserRouter>
-       <Main/>
-    </BrowserRouter>
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import NavBar from './Components/NavBar'
+import Favorites from './pages/Favorites'
+import { BrowserRouter } from 'react-router-dom'
+import { MovieProvider } from './Contexts/MovieContext'
+function App() {
     
+  return(
+   <MovieProvider>
+    <div>
+       <NavBar/>
+       <main>
+        <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/favorites" element={<Favorites/>}/>
+        </Routes>
+       </main>
+    </div>
+  </MovieProvider>
   );
 }
+
 
 export default App
