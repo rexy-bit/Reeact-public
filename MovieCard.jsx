9b097cb88e29178829cff4b712818e0a622fce15
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-03 23:24:13 +0100

    Add files via upload

--- MovieCard.jsx
+++ MovieCard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import '../css/MovieCard.css'
 import { useMovieContext } from "../Contexts/MovieContext";
 export default function MovieCard({movie}){
+  
 
     const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext();
 
-    const favorite = isFavorite(movie.id);
+    const favorite = isFavorite(movie.id)
+    
 
     function onFavoriteClick(e){
         e.preventDefault();
@@ -13,25 +15,23 @@ export default function MovieCard({movie}){
             else addToFavorites(movie)
         
     }
-
     return(
 
         <div className="movie-card">
-   
-             <div className="movie-poster">
+            <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-             </div>
+                
+            </div>
 
-             <div className="movie-overlay">
-                <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>&hearts;</button>
-             </div>
+            <div className="movie-overlay">
+                <button onClick={onFavoriteClick} className={`favorite-btn ${favorite ? "active" : ""}`}>&hearts;</button>
 
-               
-             <div className="movie-info">
+            </div>
+
+            <div className="movie-info">
                 <h3>{movie.title}</h3>
                 <p>{movie.release_date?.split("-")[0]}</p>
-             </div>
-
+            </div>
         </div>
-    )
+    );
 }
