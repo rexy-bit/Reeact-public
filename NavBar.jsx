5395442ac6149b0713784147de5532759a66aa03
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-04 23:16:31 +0100

    Add files via upload

--- NavBar.jsx
+++ NavBar.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+
 import { Link } from "react-router-dom";
-import '../css/NavBar.css'
+
+
 export default function NavBar(){
 
     return(
-        <header>
-            <h2>Movie App</h2>
+        <header className="top-0 flex flex-row w-full items-center justify-between bg-blue-950 h-[55px] px-[10px] fixed">
+            <h1 className="text-[1.8em] font-black text-indigo-500">To-Do-List</h1>
 
-            <div className="nav-links">
-                <Link to="/">Home</Link>
-                <Link to="/favorites">Favorites</Link>
-            </div>
+           <nav className="flex flex-row items-center gap-[20px] text-indigo-500 font-bold text-[1.1em] ">
+            <Link to="/" className="cursor-pointer transition-opacity duration-200 hover:opacity-80 active:opacity-60">Todos</Link>
+            <Link to="/done" className="cursor-pointer transition-opacity duration-200 hover:opacity-80 active:opacity-60">Done</Link>
+            <Link to="/undone" className="cursor-pointer transition-opacity duration-200 hover:opacity-80 active:opacity-60">Undone</Link>
+            </nav>
         </header>
     );
 }
