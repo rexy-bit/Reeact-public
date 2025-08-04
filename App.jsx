05bf25230e1873bd645fa15574cbe2bff94c2ab6
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-04 23:15:33 +0100

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,27 +1,38 @@
-import React from 'react'
-
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
+import { useState } from 'react'
 import NavBar from './Components/NavBar'
-import Favorites from './pages/Favorites'
-import { BrowserRouter } from 'react-router-dom'
-import { MovieProvider } from './Contexts/MovieContext'
+import {Routes, Route} from 'react-router-dom'
+import TodoInputs from './Components/TodoInputs'
+
+import { TodoProvider } from './Contexts/TodoContext';
+import TodoList from './Components/TodoList';
+import Done from './pages/Done';
+import Undone from './pages/Undone';
 function App() {
-    
+
   return(
-   <MovieProvider>
-    <div>
-       <NavBar/>
-       <main>
+    <TodoProvider>
+  <div className='w-full'>
+      <NavBar/>
+
+      <main className='flex flex-col justify-center items-center'>
         <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/favorites" element={<Favorites/>}/>
+          <Route path="/" element={
+               <>
+                 <TodoInputs/>
+                 <TodoList/>
+               </>
+
+          }/>
+      
+
+        
+          <Route path="/done" element={<Done/>}/>
+          <Route path="/undone" element={<Undone/>}/>
         </Routes>
-       </main>
-    </div>
-  </MovieProvider>
+      </main>
+  </div>
+  </TodoProvider>
   );
 }
-
 
 export default App
