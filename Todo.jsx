90abdc2a22bdbc689864a0e16b13d7106465d858
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-04 23:16:39 +0100

    Add files via upload

--- Todo.jsx
+++ Todo.jsx
@@ -1,50 +1,36 @@
 import React from "react";
-import TodoInputs from "./TodoInputs";
-import TodoDisplay from "./TodoDisplay";
-export default function Todo(){
+import { useTodoContext } from "../Contexts/TodoContext";
+export default function Todo({todo}){
 
-    const [todoList, setTodoList] = React.useState(()=>{
-        const saved = localStorage.getItem('todoList');
-         return saved ? JSON.parse(saved) : 
-           [
-            {
-                id : "0",
-                name : 'Go to school',
-                date : '04/09/2025',
-                done : false
-            },{
-                id : '1',
-                name : 'Make dinner',
-                date:  '05/09/2025',
-                done : false
-            }
-           ];
-    });
-
-
-    React.useEffect(()=>{
-        localStorage.setItem('todoList', JSON.stringify(todoList));
-    },
-     [todoList]);
-
-
-     return(
-
-        <section>
-            <h1>📝To-Do-List</h1>
-
-            <TodoInputs
-             todoList={todoList}
-             setTodoList={setTodoList}
-            />
-
-            <TodoDisplay
-             todoList={todoList}
-             setTodoList={setTodoList}
-            />
-        </section>
-     );
+    const {todoList, setTodoList} = useTodoContext();
     
+    function deleteTodo(todoId){
+        setTodoList(prev=> prev.filter((t)=> t.id !== todoId))
+    }
 
+    function toggleTodo(todo){
+
+        setTodoList(prev => prev.map((t)=> t.id === todo.id ? {...t, state : !t.state} : t));
+          
+    }
+
+    return(
+
+        <div className="flex flex-row w-full justify-between px-10 py-5 rounded-4xl bg-amber-500 text-[18px] items-center transition-transform duration-200 ease-in-out hover:scale-110">
+            
+                <input 
+                type="checkBox" 
+                className="w-4"
+                checked={todo.state}
+                onChange={()=>{toggleTodo(todo)}}
+                
+                />
+
+                <div>{todo.name}</div>
+                <div>{todo.date}</div>
+
+                <button className="bg-red-600 text-white px-5 py-1 rounded-3xl cursor-pointer transition-opacity duration-200 hover:opacity-70 active:opacity-50" onClick={()=>deleteTodo(todo.id)}>Delete</button>
+        </div>
+    );
 
 }
