422ce12d1e73dd81c081b8a332c64a13ee8e834f
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-07 23:23:03 +0100

    Add files via upload

--- Home.jsx
+++ Home.jsx
@@ -1,80 +1,8 @@
-import React from "react";
-import { getPopularMovies, searchMovies } from "../api";
-import MovieCard from "../Components/MovieCard";
-import '../css/Home.css'
+import HomeHeader from "../HomeComponents.jsx/HomeHeader";
+import HomeText from "../HomeComponents.jsx/HomeText";
 export default function Home(){
-
-
-    const [searchQuery, setSearchQuery] = React.useState('');
-
-    function saveSearchQuery(event){
-        const value = event.target.value;
-
-        setSearchQuery(value);
-    }
-
-    const [movies, setMovies] = React.useState([]);
-
     
-
-    React.useEffect(()=>{
-      
-      const loadPopularMovies = async () => {
-          
-      
-        try{
-            const popularMovies = await getPopularMovies();
-
-            setMovies(popularMovies);
-        }catch(err){
-            console.log('Error : ', err);
-        }
-       }
-
-       loadPopularMovies();
-    },[]);
-
-    const handleSearch = async (event) => {
-         event.preventDefault();
-
-         if(!searchQuery) return;
-
-         try{
-            const searchResult = await searchMovies(searchQuery);
-            setMovies(searchResult);
-         }catch(err){
-            console.log(err);
-
-         }
-
-    }
     return(
-
-        <div className="home">
-
-            <form className="form" onSubmit={handleSearch}>
-                <input 
-                type="text" 
-                className="search-input" 
-                placeholder="Search for movies..."
-                value={searchQuery}
-                onChange={saveSearchQuery}
-                />
-
-                <button className="search-button" type="submit">Search</button>
-
-            </form>
-
-            <div className="movies-grid">
-                {movies.map((movie)=>{
-                    return(
-                        <MovieCard
-                        movie={movie}
-                        key={movie.id}
-                        />
-                    )
-                })}
-            </div>
-        </div>
+        <HomeText/>
     )
 }
