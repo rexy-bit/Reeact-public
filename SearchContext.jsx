5ef3b5e4bdbb98376f762cd00c3de8cc12af9c84
Yanis Rezgui <yanisrezgui28@gmail.com>
2025-08-07 23:24:10 +0100

    Add files via upload

--- SearchContext.jsx
+++ SearchContext.jsx
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { Children } from "react";
 
 const SearchContext = React.createContext();
 
-export const useSearchContext = ()=> React.useContext(SearchContext);
+export const useSearchContext =() => React.useContext(SearchContext);
 
+export const SearchProvider = ({children}) => {
 
-export const SearchProvider = ({children})=> {
+    const [search, setSearch] = React.useState(()=>{
+        const saved = localStorage.getItem('search');
 
-    const [search, setSearch] = React.useState(null);
-    const [goSearch, setGoSearch] = React.useState(false);
-    const value = {
-        search,
+        return saved ? JSON.parse(saved) : 
+           ""
+    });
+
+    React.useEffect(()=>{
+        localStorage.setItem('search', JSON.stringify(search));
+    }, [search]);
+
+    const value={
+        search, 
         setSearch
-    };
-
+    }
     return(
         <SearchContext.Provider value={value}>
-              {children}
+             {children}
         </SearchContext.Provider>
-    )
+    );
 }
-
